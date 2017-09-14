d6d85c102dbf38f682d64ad13222226a784c8827
Bilhasry Ramadhony <bilhasry@gmail.com>
2017-09-14 15:16:06 +0800

    first update data

---
+++ app.js
@@ -0,0 +1,41 @@
+
+var prev= '';
+var a = angular.module("myApp", ['ngSanitize', 'ngRoute']);
+
+a.config(function($routeProvider, $locationProvider){
+	$routeProvider
+	.when("/faq", {
+		templateUrl : "faq.html",
+		controller : function( $scope){
+			
+			$scope.$on('$routeChangeSuccess', function(){
+				$('html,body').animate({
+	                       scrollTop: 0
+	                  }, 1000);
+				prev = 'faq';
+
+				$('#main-menu').collapse('hide');
+			});
+
+			
+		}
+	})
+	.otherwise({
+		templateUrl : "home.html",
+		controller : function($scope, $location){
+			
+			$scope.$on('$viewContentLoaded', function(){
+	          $('#main-menu').collapse('hide');
+	        });
+		}
+	});
+
+	
+})
+.directive('headerImage', function(){
+	return{
+		link : function( scope, element, attrs, tabsCtrl){
+			element[0].innerHTML = '<img src="assets/img/iema_2017_header.svg" />';
+		}
+	}
+});
