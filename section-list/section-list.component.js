f8e4f2ef5e620b86a554b58a477b69d4745b09c5
Bilhasry Ramadhony <bilhasry@gmail.com>
2017-10-06 11:04:52 +0800

    update with tc plugin

--- section-list/section-list.component.js
+++ section-list/section-list.component.js
@@ -95,7 +95,14 @@ a.
                         return false;
                     }
                 });
-              
+
+                (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+             js.src = "https://dev2.trustedcompany.com/statics/javascripts/plugins.js";
+              fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'tc-plugins'));
           };
       });
     }
