62bd26bfe8c6933f8f946c0ccbcdee3970e00cd5
Bilhasry Ramadhony <bilhasry@gmail.com>
2017-10-24 11:21:17 +0700

    update

--- section-list/section-list.component.js
+++ section-list/section-list.component.js
@@ -96,6 +96,10 @@ a.
                     }
                 });
 
+                $('.nav-item').on('click', function(){
+                  $('.navbar-collapse').collapse('hide');
+                });
+
           };
       });
     }
